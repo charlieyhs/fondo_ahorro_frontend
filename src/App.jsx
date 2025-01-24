8c6266482cc317bb65819fc6465fda8002ccef07
Carlos Hernández <carlossanchez25fasa@gmail.com>
2025-01-23 23:22:07 -0500

    Feat: modificación de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Home from './components/Home';
-
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() =>{
+    document.title= 'Fondo ahorro';
+  }, []);
   return (
     <Router>
       <Routes>
