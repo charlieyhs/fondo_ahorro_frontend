b5c1d91ebfbf3b3ada6f1bd20ed92f42d985a9d1
Carlos Hernández <carlossanchez25fasa@gmail.com>
2025-08-24 22:28:10 -0500

    [FEAT]: Implementación de campos especificos para la página de ratehistory

---
+++ src/components/Inputs/DatePicker.jsx
@@ -0,0 +1,52 @@
+import dayjs from 'dayjs';
+import 'dayjs/locale/es';
+import 'dayjs/locale/en';
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {DatePicker as DatePickerMUI} from '@mui/x-date-pickers/DatePicker';
+import PropTypes from 'prop-types';
+
+
+const DatePicker = ({label, slotProps, value, onChange}) => {
+    
+    const defaultSlotProps = {
+        textField: {
+            color: 'success',
+        },
+    };
+    
+    const {i18n} = useTranslation();
+    
+    useEffect(() => {
+        dayjs.locale(i18n.language);
+    },[i18n.language]);
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={i18n.language}>
+            <DatePickerMUI
+                label={label}
+                value={value ? dayjs(value) : null}
+                onChange={(newValue) => onChange(newValue)}
+                slotProps={{
+                        ...defaultSlotProps, 
+                        ...slotProps,
+                        textField: {
+                            ...defaultSlotProps.textField,
+                            ...slotProps?.textField,
+                        },
+                    }}
+                views={['year', 'month', 'day']}/>
+        </LocalizationProvider>
+    );
+};
+
+DatePicker.propTypes = {
+    label : PropTypes.string.isRequired,
+    slotProps : PropTypes.object,
+    value : PropTypes.object,
+    onChange : PropTypes.func
+}
+
+export default DatePicker;
---
+++ src/pages/MoneyContributions.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/menus/Sidebar";
+
+const MoneyContributions = () => {
+    return (
+        <div className='divPag'>
+            <Sidebar />
+            <h1>Contribuciones de dinero al fondo</h1>
+        </div>
+    );
+};
+
+export default MoneyContributions;
